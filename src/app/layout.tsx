1bdf0aa5b9446c2d1e5b401150068f323964e57c
devin <80857978+notdevin-1@users.noreply.github.com>
2025-10-18 22:53:21 -0700

    meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Stang Hacks",
   description: "Monte Vista High School's first hackathon",
+  themeColor: "#b91c1c",
 };
 
 export default function RootLayout({
